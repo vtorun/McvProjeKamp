1c3b4a53d6575dd282840f1c08e195e1b7c4759d
vtorun <64329150+vtorun@users.noreply.github.com>
2021-06-20 17:55:03 +0300

    Commit

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> genericRepository = new GenericRepository<Category>();
+        public List<Category> getAll()
+        {
+            return genericRepository.List();
+        }
+        public void CategoryAdd(Category category)
+        {
+            if (category.CategoryName == "" || category.CategoryName.Length <= 3 ||
+                    category.CategoryDescription == "" || category.CategoryName.Length >= 50)
+            {
+            }
+            else
+            {
+                genericRepository.Add(category);
+            }
+        }
+    }
+}
